cdd0f33642f46ad8fc7ee0ecd671f99108842f4e
IsaacToluwani <57397001+IsaacToluwani@users.noreply.github.com>
2022-05-13 13:03:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//* selecting elements
+
+const display = document.querySelector(".display");
+const btn7 = document.querySelector(".btn-7");
+const btn8 = document.querySelector(".btn-8");
+const btn9 = document.querySelector(".btn-9");
+const btnDel = document.querySelector(".btn-del");
+const btn4 = document.querySelector(".btn-4");
+const btn5 = document.querySelector(".btn-5");
+const btn6 = document.querySelector(".btn-6");
+const btnPlus = document.querySelector(".btn-plus");
+const btn1 = document.querySelector(".btn-1");
+const btn2 = document.querySelector(".btn-2");
+const btn3 = document.querySelector(".btn-3");
+const btnMinus = document.querySelector(".btn-minus");
+const btnDecimalPoint = document.querySelector(".btn-decimal-point");
+const btn0 = document.querySelector(".btn-0");
+const btnDivision = document.querySelector(".btn-division");
+const btnMultiplication = document.querySelector(".btn-multiplication");
+const btnReset = document.querySelector(".btn-reset");
+const btnReturn = document.querySelector(".btn-return");
